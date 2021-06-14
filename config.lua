7b713ae9abe59f67a942d9015176d43b598a9534
TheFiveMCoders <44032346+thefivemcoders@users.noreply.github.com>
2021-06-14 11:07:25 +0530

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config              = {}
+Config.MarkerType   = 0
+Config.DrawDistance = 100.0
+Config.ZoneSize     = {x = 1.0, y = 2.0, z = 1.0}
+Config.MarkerColor  = {r = 0, g = 255, b = 0}
+Config.ShowBlips   = true  
+
+Config.RequiredCopsKoda  = 0
+
+Config.TimeToFarm    = 1 * 10000
+Config.TimeToProcess = 1 * 10000
+Config.TimeToSell    = 1  * 10000
+
+Config.Locale = 'en'
+
+Config.Zones = {
+	Catchapple =		{x = 328.64,	y = 6530.87,	z = 28.69,	name = _U('Apple_picking'),		sprite = 570,	color = 47},
+	applejuice =	{x = -368.41,	y = 6335.54,	z = 29.85,	name = _U('turns_from_juice'),	sprite = 93,	color = 47},
+	Sellapplejuice =		{x = 1590.78,	y = 6451.25,	z = 25.32,	name = _U('sell_juice_apple_blip'),		sprite = 605,	color = 47}
+}
---
+++ esx_applefarm.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (name, label, `limit`) VALUES
+	('apple', 'Apple', 50),
+	('juice_apple', 'Apple juice', 10)
+;
